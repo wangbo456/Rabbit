db0a34ab4fc1e0fde95146014f629445f2f717e9
zzy970910 <1498162732@qq.com>
2018-07-03 10:16:03 +0800

    张正义

--- src/main/java/com/zm/controller/LoyalController.java
+++ src/main/java/com/zm/controller/LoyalController.java
@@ -46,6 +46,6 @@ public class LoyalController {
      * @Date: 2018/7/3 9:58
      */
     public static void getABC(){
-        System.out.println("456");
+        System.out.println("123");
     }
 }
