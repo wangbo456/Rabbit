c5187bdcf1ef9422ab73b68ee99b0ccb720c8dc7
gzw2060 <1224199208@qq.com>
2018-07-03 09:04:01 +0800

    1

---
+++ src/main/java/com/zm/controller/LoyalController.java
@@ -0,0 +1,21 @@
+package com.zm.controller;
+
+/**
+ * @program: Rabbit2
+ * @description: ${description}
+ * @author: 传闻中的郭先森
+ * @create: 2018-07-03 08:50
+ **/
+public class LoyalController {
+
+    /**
+     * 功能描述:〈This is Main test〉
+     * @Author:传闻中的郭先森
+     * @Date: 2018/7/3 8:51
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
